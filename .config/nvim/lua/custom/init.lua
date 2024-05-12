13c9f49598cab3b490a211de5d2766cb9823041c
Rahul Yedida <rahul.yedida@pm.me>
2024-05-12 01:28:22 -0400

    Open nvim-tree auto

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -4,3 +4,11 @@ vim.opt.foldenable = true
 
 vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
 vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
