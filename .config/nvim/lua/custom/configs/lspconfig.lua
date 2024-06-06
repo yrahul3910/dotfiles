3f1a204182d25d74107423934e3fa1ebe207e5c1
Rahul Yedida <yrahul3910@gmail.com>
2024-06-06 11:12:06 -0400

    Commented out pyright, ruff does it better

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,8 +1,11 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {
-  filetypes = { 'python', 'pysh' }
-}
+-- lspconfig.pyright.setup {
+--   filetypes = { 'python', 'pysh' },
+--   root_dir = function()
+--     return vim.fn.getcwd()
+--   end
+-- }
 lspconfig.tsserver.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
