0067ff72b7f80de18af63036e172f666f41f4d90
Rahul Yedida <yrahul3910@gmail.com>
2024-06-11 10:29:35 -0400

    Fix config issue in auto-session

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -84,7 +84,7 @@ local plugins = {
       require("auto-session").setup {
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-        auto_session_enable_last_session = true,
+        auto_session_enable_last_session = false,
         session_lens = {
           -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
           buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
