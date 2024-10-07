9bbf7f8533d65f6e7336af35705f601e9a0f5aed
Rahul Yedida <yrahul3910@gmail.com>
2024-10-07 16:35:40 -0400

    Harpoon2: remove Telescope integration

--- .config/nvim/lua/custom/mappings/harpoon.lua
+++ .config/nvim/lua/custom/mappings/harpoon.lua
@@ -7,24 +7,6 @@ local function map_normal(keys, mapping, desc)
   vim.keymap.set('n', keys, mapping, { desc = desc, noremap = true, silent = true })
 end
 
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Harpoon2',
-      finder = require('telescope.finders').new_table {
-        results = file_paths,
-      },
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
-    })
-    :find()
-end
-
 map_normal('<leader>pha', function()
   harpoon:list():add()
 end, 'Harpoon2: add')
@@ -34,7 +16,7 @@ map_normal('<leader>phd', function()
 end, 'Harpoon2: remove')
 
 map_normal('<leader>pho', function()
-  toggle_telescope(harpoon:list())
+  harpoon.ui:toggle_quick_menu(harpoon:list())
 end, 'Harpoon2: open')
 
 map_normal('<C-m>', function()
