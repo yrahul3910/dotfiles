a7b3c72a2284870dc3fa6eed82d5b414b20a30c1
Rahul Yedida <yrahul3910@gmail.com>
2024-10-19 22:44:10 -0400

    Rename map function
    Renames map_normal to nmap by introducing a more general version and
    specializing it, also allowing for vmap.

--- .config/nvim/lua/custom/mappings/mappings.lua
+++ .config/nvim/lua/custom/mappings/mappings.lua
@@ -1,92 +1,107 @@
-local function map_normal(keys, mapping, desc)
-  vim.keymap.set('n', keys, mapping, { desc = desc, noremap = true, silent = true })
+local map = function(defaults)
+  local config = vim.tbl_deep_extend('force', { mode = {}, prefix = '', opts = {} }, defaults)
+  return function(lhs, rhs, desc, opts)
+    lhs = config.prefix .. lhs
+    opts = opts or {}
+    if type(desc) == 'table' then
+      opts = desc
+    else
+      opts.desc = desc
+    end
+    opts = vim.tbl_deep_extend('force', config.opts, opts)
+    vim.keymap.set(config.mode, lhs, rhs, opts)
+  end
 end
 
+local nmap = map { mode = 'n' }
+local vmap = map { mode = 'v' }
+
 -- Custom
-map_normal('<leader>o', 'o<Esc>S<Esc>k', 'New empty line below')
-map_normal('<leader>O', 'O<Esc>S<Esc>j', 'New empty line above')
-map_normal('n', 'nzzzv', 'Next') -- From LazyVim, centers the screen after jumping
-map_normal('<leader>gcu', 'dd/|||<CR>0v/>>><CR>$x', '[G]it [C]onflict Choose [U]pstream')
-map_normal('<leader>gcb', '0v/|||<CR>$x/====<CR>0v/>>><CR>$x', '[G]it [C]onflict Choose [B]ase')
-map_normal('<leader>gcs', '0v/====<CR>$x/>>><CR>dd', '[G]it [C]onflict Choose [S]tashed')
+nmap('<leader>o', 'o<Esc>S<Esc>k', 'New empty line below')
+nmap('<leader>O', 'O<Esc>S<Esc>j', 'New empty line above')
+nmap('n', 'nzzzv', 'Next') -- From LazyVim, centers the screen after jumping
+nmap('<leader>fc', '/<<<<CR>', '[F]ind [C]onflicts')
+nmap('<leader>gcu', 'dd/|||<CR>0v/>>><CR>$x', '[G]it [C]onflict Choose [U]pstream')
+nmap('<leader>gcb', '0v/|||<CR>$x/====<CR>0v/>>><CR>$x', '[G]it [C]onflict Choose [B]ase')
+nmap('<leader>gcs', '0v/====<CR>$x/>>><CR>dd', '[G]it [C]onflict Choose [S]tashed')
 
 -- Some mappings inspired by Helix (Kakoune mappings)
-map_normal('gh', '^', 'Move to first word') -- Somewhat different from helix
-map_normal('gl', '$b', 'Move to end of line')
-map_normal('ge', 'G', 'Move to end of file')
+nmap('gh', '^', 'Move to first word') -- Somewhat different from helix
+nmap('gl', '$b', 'Move to end of line')
+nmap('ge', 'G', 'Move to end of file')
 
 -- Convenience mappings
-map_normal('gb', 'kJi', 'Go back (to prev line)')
-map_normal('<leader>lr', ':LspRestart<CR>', '[L]SP [R]estart')
+nmap('gb', 'kJi', 'Go back (to prev line)')
+nmap('<leader>lr', ':LspRestart<CR>', '[L]SP [R]estart')
 
 -- Mappings from ThePrimeagen
 -- Lets you move selected block around in visual mode
-vim.keymap.set('v', 'K', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set('v', 'J', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vmap('K', ":m '>+1<CR>gv=gv", 'Move selection down')
+vmap('J', ":m '<-2<CR>gv=gv", 'Move selection up')
 
 -- Keeps your cursor steady when moving up and down half-pages
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+nmap('<C-d>', '<C-d>zz', 'Move down half a page')
+nmap('<C-u>', '<C-u>zz', 'Move up half a page')
 
-vim.keymap.set('n', 'Q', '<nop>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>xc', '<cmd>!chmod +x %<CR>', { noremap = true, silent = true, desc = 'Make executable' })
+nmap('Q', '<nop>')
+nmap('<leader>xc', '<cmd>!chmod +x %<CR>', 'Make executable')
 -- End Primeagen mappings
 
 -- Swap j and k
-vim.api.nvim_set_keymap('n', 'j', 'k', { noremap = true })
-vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gj', 'gk', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gk', 'gj', { noremap = true })
-vim.api.nvim_set_keymap('v', 'j', 'k', { noremap = true })
-vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true })
+nmap('j', 'k')
+nmap('k', 'j')
+nmap('gj', 'gk')
+nmap('gk', 'gj')
+nmap('j', 'k')
+nmap('k', 'j')
 
 -- Optional, but nice-to-haves
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gj' : 'j'", { expr = true, noremap = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gk' : 'k'", { expr = true, noremap = true })
+nmap('k', "v:count == 0 ? 'gj' : 'j'", 'Go down', { expr = true })
+nmap('j', "v:count == 0 ? 'gk' : 'k'", 'Go up', { expr = true })
 
 -- Delete without copying
-vim.keymap.set('n', '<leader>d', '"_d', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>c', '"_c', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>yc', '"*y', { noremap = true, silent = true, desc = '[Y]ank to [C]lipboard' })
+nmap('<leader>d', '"_d')
+nmap('<leader>c', '"_c')
+vmap('<leader>c', '"_c')
+vmap('<leader>yc', '"*y', '[Y]ank to [C]lipboard')
 
 -- fzf-lua
 local fzf = require 'fzf-lua'
-map_normal('<leader>ff', function()
+nmap('<leader>ff', function()
   fzf.files { fzf_opts = { ['--keep-right'] = '' } }
 end, '[F]ind [F]iles')
-map_normal('<leader>fg', function()
+nmap('<leader>fg', function()
   fzf.live_grep_native { fzf_opts = { ['--keep-right'] = '' } }
 end, '[F]ind by [G]rep')
 
-map_normal('<leader>fb', fzf.buffers, '[F]ind [B]uffers')
-map_normal('<leader>fc', '/<<<<CR>', '[F]ind [C]onflicts')
-map_normal('<leader>fr', fzf.live_grep_resume, '[F]ind [R]esume')
-map_normal('<leader>fG', fzf.live_grep_glob, '[F]ind by grep with [G]lob')
-map_normal('<leader>fh', fzf.helptags, '[F]ind [H]elp')
-map_normal('<leader>fd', fzf.diagnostics_document, '[F]ind [D]iagnostics')
-map_normal('<leader>fs', fzf.lsp_document_symbols, '[F]ind [S]ymbols')
-map_normal('<leader>ca', fzf.lsp_code_actions, '[C]ode [A]ctions')
-map_normal('<leader>gd', fzf.lsp_definitions, '[G]o to [D]efinition')
+nmap('<leader>fb', fzf.buffers, '[F]ind [B]uffers')
+nmap('<leader>fr', fzf.live_grep_resume, '[F]ind [R]esume')
+nmap('<leader>fG', fzf.live_grep_glob, '[F]ind by grep with [G]lob')
+nmap('<leader>fh', fzf.helptags, '[F]ind [H]elp')
+nmap('<leader>fd', fzf.diagnostics_document, '[F]ind [D]iagnostics')
+nmap('<leader>fs', fzf.lsp_document_symbols, '[F]ind [S]ymbols')
+nmap('<leader>ca', fzf.lsp_code_actions, '[C]ode [A]ctions')
+nmap('<leader>gd', fzf.lsp_definitions, '[G]o to [D]efinition')
 
 -- nvim-tree
-map_normal('<C-s>', '<cmd>NvimTreeToggle<CR>', 'Toggle NvimTree')
-map_normal('<leader>pTr', '<cmd>NvimTreeRefresh<CR>', 'Refresh NvimTree')
-map_normal('<leader>pTm', '<cmd>NvimTreeResize -10<CR>', 'nvim-tree [m]inus')
-map_normal('<leader>pTp', '<cmd>NvimTreeResize +10<CR>', 'nvim-tree [p]lus')
+nmap('<C-s>', '<cmd>NvimTreeToggle<CR>', 'Toggle NvimTree')
+nmap('<leader>ptr', '<cmd>NvimTreeRefresh<CR>', 'Refresh NvimTree')
+nmap('<leader>ptm', '<cmd>NvimTreeResize -10<CR>', 'nvim-tree [m]inus')
+nmap('<leader>ptp', '<cmd>NvimTreeResize +10<CR>', 'nvim-tree [p]lus')
 
 -- aerial
-map_normal('<leader>a', '<cmd>AerialToggle!<CR>', 'Toggle Aerial window')
+nmap('<leader>a', '<cmd>AerialToggle!<CR>', 'Toggle Aerial window')
 
 -- neotest
-map_normal('<leader>tr', function()
+nmap('<leader>tr', function()
   require('neotest').run.run()
 end, 'neotest: Run nearest')
 
-map_normal('<leader>tf', function()
+nmap('<leader>tf', function()
   require('neotest').run.run(vim.fn.expand '%')
 end, 'neotest: Test file')
 
-map_normal('<leader>to', function()
+nmap('<leader>to', function()
   require('neotest').output.open()
 end, 'neotest: Open output')
 
@@ -98,9 +113,9 @@ end
 vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
 
 -- bufferline + scope
-map_normal('<tab>', '<cmd>bnext<CR>', 'Next buffer')
-map_normal('<S-tab>', '<cmd>bprev<CR>', 'Previous buffer')
-map_normal('<leader>bc', '<cmd>bdelete<CR><cmd>bprevious<CR>', '[B]uffer [C]lose')
+nmap('<tab>', '<cmd>bnext<CR>', 'Next buffer')
+nmap('<S-tab>', '<cmd>bprev<CR>', 'Previous buffer')
+nmap('<leader>bc', '<cmd>bdelete<CR><cmd>bprevious<CR>', '[B]uffer [C]lose')
 
 -- nvim-spider
 vim.keymap.set({ 'n', 'o', 'x' }, 'W', '<cmd>lua require("spider").motion("w")<CR>', { desc = 'Spider-w' })
@@ -108,10 +123,10 @@ vim.keymap.set({ 'n', 'o', 'x' }, 'E', '<cmd>lua require("spider").motion("e")<C
 vim.keymap.set({ 'n', 'o', 'x' }, 'B', '<cmd>lua require("spider").motion("b")<CR>', { desc = 'Spider-b' })
 
 -- theme
-map_normal('<leader>cl', function()
+nmap('<leader>cl', function()
   vim.cmd 'colorscheme catppuccin'
 end, '[C]olorscheme [L]ight')
-map_normal('<leader>cd', function()
+nmap('<leader>cd', function()
   vim.cmd 'colorscheme arctic'
 end, '[C]olorscheme [D]ark')
 
@@ -127,4 +142,4 @@ end, '[C]olorscheme [D]ark')
 --   vim.cmd 'normal! x'
 -- end
 --
--- map_normal('<LeftMouse>', '<cmd>lua totally_harmless_dont_worry()<CR>')
+-- nmap('<LeftMouse>', '<cmd>lua totally_harmless_dont_worry()<CR>')
