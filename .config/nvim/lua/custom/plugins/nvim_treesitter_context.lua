e493f6cd881cfd8c1ef62f4bb3e6f3efdfebb908
Rahul Yedida <yrahul3910@gmail.com>
2025-01-29 23:30:55 -0500

    Lazy-load more modules

--- .config/nvim/lua/custom/plugins/nvim_treesitter_context.lua
+++ .config/nvim/lua/custom/plugins/nvim_treesitter_context.lua
@@ -1,3 +1,7 @@
-return { 'nvim-treesitter/nvim-treesitter-context', dependencies = { 'nvim-treesitter/nvim-treesitter' }, opts = {
-  max_lines = 10,
-} }
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    max_lines = 10,
+  },
+}
--- .config/nvim/lua/custom/plugins/typescript_tools.lua
+++ .config/nvim/lua/custom/plugins/typescript_tools.lua
@@ -1,5 +1,6 @@
 return {
   'pmizio/typescript-tools.nvim',
+  ft = { 'typescript', 'typescriptreact' },
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {},
 }
