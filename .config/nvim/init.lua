5778c92937a9fb3f7da9a4bb11059f56bf9120c1
Rahul Yedida <yrahul3910@gmail.com>
2025-02-28 02:04:13 -0500

    fix(neovim): downgrade gitsigns to working version

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -291,6 +291,7 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    version = '1.0.1',
     opts = {
       signs = {
         add = { text = '+' },
