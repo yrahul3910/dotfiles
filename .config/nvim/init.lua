790134085b2fb83a969376b37bc4322de0294678
Rahul Yedida <yrahul3910@gmail.com>
2025-03-03 20:42:46 -0500

    chore(neovim): minor cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -431,9 +431,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
 
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
-      -- vim.keymap.set('n', '<leader>ff', function()
-      --   builtin.find_files { no_ignore = true }
-      -- end, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
