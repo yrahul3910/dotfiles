0b77b2886992e26f43c8c8e092129573c165e232
Rahul Yedida <yrahul3910@gmail.com>
2025-03-24 20:47:11 -0400

    fix(neovim): fix latexmk infinite loop

--- .config/nvim/lua/custom/plugins/vimtex.lua
+++ .config/nvim/lua/custom/plugins/vimtex.lua
@@ -4,5 +4,15 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+      ignore_additional = { 'svg-inkscape/' },
+    }
   end,
 }
---
+++ .latexmkrc
@@ -0,0 +1,2 @@
+$clean_ext = "svg-inkscape/* %R-inkscape.* %R.synctex.gz";
+$hash_calc_ignore_pattern{'pdf'}='^';
