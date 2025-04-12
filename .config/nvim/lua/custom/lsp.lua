2098bb2ed498f10f78c6982a140bcef9cd9f59b6
Rahul Yedida <yrahul3910@gmail.com>
2025-04-12 16:37:53 -0400

    feat(neovim): add inlay hints

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -110,8 +110,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --
     -- This may be unwanted, since they displace some of your code
     if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+      -- Enable inlay hints by default
+      vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+      
       map('<leader>th', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }), { bufnr = event.buf })
       end, '[T]oggle Inlay [H]ints')
     end
   end,
