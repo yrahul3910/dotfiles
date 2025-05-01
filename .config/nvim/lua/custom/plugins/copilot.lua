add4a8bb57dbe2f7ac42fa9ee5713e01116a0074
Rahul Yedida <yrahul3910@gmail.com>
2025-05-01 15:40:18 -0400

    feat(neovim): fix Copilot/blink

--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -1,44 +1,34 @@
 if _G.myconfig.copilot_enabled then
   return {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    build = ':Copilot auth',
-    event = 'InsertEnter',
-    config = function()
-      require('copilot').setup {
-        panel = {
-          enabled = true,
-          auto_refresh = true,
-          keymap = {
-            jump_prev = '[[',
-            jump_next = ']]',
-            accept = '<CR>',
-            open = '<M-CR>',
+    {
+      'zbirenbaum/copilot.lua',
+      cmd = 'Copilot',
+      build = ':Copilot auth',
+      event = 'InsertEnter',
+      config = function()
+        require('copilot').setup {
+          panel = {
+            enabled = true,
+            auto_refresh = true,
+            keymap = {
+              jump_prev = '[[',
+              jump_next = ']]',
+              accept = '<CR>',
+              open = '<M-CR>',
+            },
           },
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          accept = false, -- disable built-in keymapping
-        },
-        copilot_model = 'gpt-4o-copilot', -- default is "gpt-3.5-turbo"
-      }
-
-      -- make sure blink and copilot don't interfere with e/o
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'BlinkCmpMenuOpen',
-        callback = function()
-          vim.b.copilot_suggestion_hidden = true
-        end,
-      })
-
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'BlinkCmpMenuClose',
-        callback = function()
-          vim.b.copilot_suggestion_hidden = false
-        end,
-      })
-    end,
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            accept = false, -- disable built-in keymapping
+          },
+          copilot_model = 'gpt-4o-copilot', -- default is "gpt-3.5-turbo"
+        }
+      end,
+    },
+    {
+      'giuxtaposition/blink-cmp-copilot',
+    },
   }
 else
   return {}
