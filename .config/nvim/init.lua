6747ad900569ca81605f50c800eeb73924534970
Rahul Yedida <yrahul3910@gmail.com>
2025-07-07 13:57:11 -0400

    fix(neovim): correct fix for markview-treesitter integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -538,10 +538,11 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     branch = 'master',
-    lazy = true,
+    lazy = false,
     event = { 'InsertEnter', 'BufWinEnter' },
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    dependencies = { 'OXY2DEV/markview.nvim' },
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'gitcommit' },
--- .config/nvim/lua/custom/plugins/markview.lua
+++ .config/nvim/lua/custom/plugins/markview.lua
@@ -1,6 +1,7 @@
 return {
   'OXY2DEV/markview.nvim',
-  lazy = true,
+  lazy = false,
+  priority = 49,
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
