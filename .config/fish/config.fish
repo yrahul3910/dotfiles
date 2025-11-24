77c49c733f666d9a526ce564d8a36a1f2e68ffb3
Rahul Yedida <yrahul3910@gmail.com>
2025-11-23 20:16:27 -0500

    feat(sh): add go executables to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -139,4 +139,6 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 set -x PATH $HOME/.local/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+
 alias claude="$HOME/.claude/local/claude"
