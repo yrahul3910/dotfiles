c9ce7a642895d55f155a016e4f1931a7100a3998
Rahul Yedida <yrahul3910@gmail.com>
2026-01-04 19:56:06 -0500

    fix(fish): fix mode for rev-i-search

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -49,7 +49,7 @@ function reverse_history_search
 end
 
 function fish_user_key_bindings
-  bind -M default / reverse_history_search
+  bind -M normal / reverse_history_search
   bind -M insert ctrl-y accept-autosuggestion
 end
 
