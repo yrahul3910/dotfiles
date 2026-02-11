ee56a36a295f6171e5079368fac3c4f56c3e52bb
Rahul Yedida <yrahul3910@gmail.com>
2026-02-10 20:04:17 -0500

    chore(neovim): remove copilot

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -64,21 +64,6 @@ return {
     'ibhagwan/fzf-lua',
     'nvim-tree/nvim-web-devicons',
     'folke/snacks.nvim',
-    'zbirenbaum/copilot.lua',
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-      },
-    },
     {
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {
