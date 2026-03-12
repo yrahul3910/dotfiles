f8df7558ecc5e689592cf5cfab734c8c1ac76ba2
Rahul Yedida <rahul@ryedida.me>
2026-03-12 13:50:24 -0400

    fix(neovim): update <leader>s which-key label

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -14,7 +14,7 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>l', group = '[L]aTeX / [L]SP' },
       { '<leader>p', group = '[P]lugins' },
       { '<leader>r', group = '[R]ename' },
-      { '<leader>s', group = '[S]idekick' },
+      { '<leader>s', group = '[S]ubstitute' },
       { '<leader>t', group = '[T]oggle / [T]est' },
       { '<leader>w', group = '[W]orkspace' },
       { '<leader>y', group = '[Y]ank' },
