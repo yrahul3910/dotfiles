bbe1f9843034c1a97ea515a58a3655b285cbcdac
Rahul Yedida <rahul@ryedida.me>
2026-03-12 13:44:56 -0400

    chore(neovim): update which-key labels, remove fancydiagnostics

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -7,15 +7,18 @@ return { -- Useful plugin to show you pending keybinds.
     -- Document existing key chains
     require('which-key').add {
       { '<leader>a', group = '[A]I' },
+      { '<leader>b', group = '[B]uffer' },
+      { '<leader>D', group = '[D]ebug' },
+      { '<leader>f', group = '[F]ind' },
+      { '<leader>G', group = '[G]it Diffview' },
+      { '<leader>g', group = '[G]it' },
+      { '<leader>l', group = '[L]aTeX / [L]SP' },
       { '<leader>p', group = '[P]lugins' },
       { '<leader>r', group = '[R]ename' },
-      { '<leader>f', group = '[F]ind' },
       { '<leader>s', group = '[S]ubstitute' },
+      { '<leader>t', group = '[T]oggle / [T]est' },
       { '<leader>w', group = '[W]orkspace' },
-      { '<leader>t', group = '[T]oggle' },
-      { '<leader>D', group = '[D]ebug' },
-      -- { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      { '<leader>l', group = '[L]aTeX / [L]SP' },
+      { '<leader>y', group = '[Y]ank' },
     }
   end,
 }
