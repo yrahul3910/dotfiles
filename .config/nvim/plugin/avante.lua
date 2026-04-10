120bf88f243458a0851ee70efc2f96b2e7558ac3
Rahul Yedida <yrahul3910@gmail.com>
2026-04-10 18:25:20 -0400

    chore(neovim): feature-flag avante

--- .config/nvim/plugin/avante.lua
+++ .config/nvim/plugin/avante.lua
@@ -1,82 +1,84 @@
-vim.pack.add({
-  {
-    src = 'https://github.com/nvim-lua/plenary.nvim',
-  },
-  {
-    src = 'https://github.com/MunifTanjim/nui.nvim',
-  },
-  {
-    src = 'https://github.com/ibhagwan/fzf-lua',
-  },
-  {
-    src = 'https://github.com/nvim-tree/nvim-web-devicons',
-  },
-  {
-    src = 'https://github.com/folke/snacks.nvim',
-  },
-  {
-    src = 'https://github.com/zbirenbaum/copilot.lua',
-  },
-  {
-    src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-  },
-  {
-    src = 'https://github.com/yetone/avante.nvim',
-  },
-})
+if _G.myconfig.copilot_enabled then
+  vim.pack.add {
+    {
+      src = 'https://github.com/nvim-lua/plenary.nvim',
+    },
+    {
+      src = 'https://github.com/MunifTanjim/nui.nvim',
+    },
+    {
+      src = 'https://github.com/ibhagwan/fzf-lua',
+    },
+    {
+      src = 'https://github.com/nvim-tree/nvim-web-devicons',
+    },
+    {
+      src = 'https://github.com/folke/snacks.nvim',
+    },
+    {
+      src = 'https://github.com/zbirenbaum/copilot.lua',
+    },
+    {
+      src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+    },
+    {
+      src = 'https://github.com/yetone/avante.nvim',
+    },
+  }
 
-require('render-markdown').setup {}
+  require('render-markdown').setup {}
 
-require('avante').setup {
-  instructions_file = 'AGENTS.md',
-  provider = 'opencode',
-  acp_providers = {
-    ['opencode'] = {
-      command = 'opencode',
-      args = { 'acp' },
+  require('avante').setup {
+    instructions_file = 'AGENTS.md',
+    provider = 'opencode',
+    acp_providers = {
+      ['opencode'] = {
+        command = 'opencode',
+        args = { 'acp' },
+      },
     },
-  },
-  disabled_tools = {
-    'rag_search',
-    'python',
-    'git_diff',
-    'git_commit',
-    'delete_path',
-  },
-  behaviour = {
-    auto_suggestions = false,
-    auto_approve_tool_permissions = false,
-    auto_apply_diff_after_generation = false,
-    confirmation_ui_style = 'popup',
-  },
-  mappings = {
-    diff = {
-      next = ']h',
-      prev = '[h',
+    disabled_tools = {
+      'rag_search',
+      'python',
+      'git_diff',
+      'git_commit',
+      'delete_path',
     },
-    suggestion = {
-      accept = '<M-y>',
-      dismiss = '<M-n>',
+    behaviour = {
+      auto_suggestions = false,
+      auto_approve_tool_permissions = false,
+      auto_apply_diff_after_generation = false,
+      confirmation_ui_style = 'popup',
     },
-    jump = {
-      next = ']]',
-      prev = '[[',
+    mappings = {
+      diff = {
+        next = ']h',
+        prev = '[h',
+      },
+      suggestion = {
+        accept = '<M-y>',
+        dismiss = '<M-n>',
+      },
+      jump = {
+        next = ']]',
+        prev = '[[',
+      },
+      submit = {
+        normal = '<CR>',
+        insert = '<C-s>',
+      },
+      cancel = {
+        normal = { '<C-c>', '<Esc>', 'q' },
+        insert = { '<C-c>' },
+      },
+      sidebar = {
+        apply_all = 'A',
+        apply_cursor = 'a',
+        edit_user_request = 'e',
+        remove_file = 'd',
+        add_file = '@',
+        close = { '<Esc>', 'q' },
+      },
     },
-    submit = {
-      normal = '<CR>',
-      insert = '<C-s>',
-    },
-    cancel = {
-      normal = { '<C-c>', '<Esc>', 'q' },
-      insert = { '<C-c>' },
-    },
-    sidebar = {
-      apply_all = 'A',
-      apply_cursor = 'a',
-      edit_user_request = 'e',
-      remove_file = 'd',
-      add_file = '@',
-      close = { '<Esc>', 'q' },
-    },
-  },
-}
+  }
+end
