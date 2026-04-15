15fa696a7b15c153cad500658e34e4faae5a92ba
Rahul Yedida <yrahul3910@gmail.com>
2026-04-15 18:32:08 -0400

    feat(terraform): use prebuilt image for deep learning

--- terraform/common.tf
+++ terraform/common.tf
@@ -2,5 +2,7 @@ provider "google" {
   project = "modified-legacy-186817"
   region  = "us-central1"
   zone    = "us-central1-a"
-  credentials = file("~/gcloud-compute-servicekey.json")
 }
+
+data "google_project" "project" {}
+
