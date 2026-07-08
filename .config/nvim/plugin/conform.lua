9fbab63e3c6c0265270a70e2dde69ff294f24b6c
Rahul Yedida <yrahul3910@gmail.com>
2026-07-08 16:50:03 -0700

    chore(neovim): update conform config for tmux

--- .config/nvim/plugin/conform.lua
+++ .config/nvim/plugin/conform.lua
@@ -20,8 +20,8 @@ require('conform').setup {
   formatters_by_ft = {
     lua = { 'stylua' },
     javascript = { 'prettier', stop_after_first = true },
-    typescript = { 'prettier', stop_after_first = true },
-    typescriptreact = { 'prettier', stop_after_first = true },
+    typescript = { 'prettier', 'biome', stop_after_first = false },
+    typescriptreact = { 'prettier', 'biome', stop_after_first = false },
     python = { 'black', 'ruff', stop_after_first = false },
     json = { 'prettier', stop_after_first = false },
     c = { 'clang-format', stop_after_first = true },
